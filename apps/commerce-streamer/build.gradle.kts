c8a1b14ecf55d1d5a7973567998a356e9312eb4b
hgh1472 <hgh1472@naver.com>
2025-09-01 01:25:57 +0900

    chore: 카프카 모듈 추가

---
+++ apps/commerce-streamer/build.gradle.kts
@@ -0,0 +1,22 @@
+dependencies {
+    // add-ons
+    implementation(project(":modules:jpa"))
+    implementation(project(":modules:redis"))
+    implementation(project(":modules:kafka"))
+    implementation(project(":supports:jackson"))
+    implementation(project(":supports:logging"))
+    implementation(project(":supports:monitoring"))
+
+    // web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // querydsl
+    implementation("com.querydsl:querydsl-jpa::jakarta")
+
+
+    // test-fixtures
+    testImplementation(testFixtures(project(":modules:jpa")))
+    testImplementation(testFixtures(project(":modules:redis")))
+    testImplementation("org.awaitility:awaitility:4.2.0")
+}
---
+++ apps/commerce-streamer/src/main/java/com/loopers/interfaces/consumer/DemoKafkaConsumer.java
@@ -0,0 +1,21 @@
+package com.loopers.interfaces.consumer;
+
+import com.loopers.config.kafka.KafkaConfig;
+import java.util.List;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoKafkaConsumer {
+
+    @KafkaListener(
+            topics = {"${demo-kafka.test.topic-name}"},
+            containerFactory = KafkaConfig.BATCH_LISTENER
+    )
+    public void demoListener(List<ConsumerRecord<Object, Object>> messages, Acknowledgment acknowledgment) {
+        System.out.println(messages);
+        acknowledgment.acknowledge(); // manual ack
+    }
+}
