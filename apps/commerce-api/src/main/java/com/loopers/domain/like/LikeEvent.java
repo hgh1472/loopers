e376784780ddc8e3f8edf467aa595714bff7102d
hgh1472 <hgh1472@naver.com>
2025-09-03 02:50:51 +0900

    refactor: 글로벌 이벤트 분리

--- apps/commerce-api/src/main/java/com/loopers/domain/like/LikeEvent.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/like/LikeEvent.java
@@ -4,10 +4,6 @@ import com.loopers.support.event.UserActivityEvent;
 import java.util.UUID;
 
 public class LikeEvent {
-    public static final class TOPIC {
-        public static final String LIKED = "internal.liked-events.v1";
-        public static final String LIKE_CANCELED = "internal.like-canceled-events.v1";
-    }
 
     public record Liked(
             UUID eventId,
--- apps/commerce-api/src/main/java/com/loopers/infrastructure/like/LikeKafkaEventPublisher.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/like/LikeKafkaEventPublisher.java
@@ -2,6 +2,7 @@ package com.loopers.infrastructure.like;
 
 import com.loopers.domain.like.LikeEvent;
 import com.loopers.domain.like.LikeGlobalEventPublisher;
+import java.time.ZonedDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -13,11 +14,15 @@ public class LikeKafkaEventPublisher implements LikeGlobalEventPublisher {
 
     @Override
     public void publish(LikeEvent.Liked event) {
-        kafkaTemplate.send(LikeEvent.TOPIC.LIKED, event.productId().toString(), event);
+        LikeGlobalEvent.Liked kafkaEvent =
+                new LikeGlobalEvent.Liked(event.eventId(), event.productId(), event.userId(), ZonedDateTime.now());
+        kafkaTemplate.send(LikeGlobalEvent.TOPIC.LIKED, kafkaEvent.productId().toString(), kafkaEvent);
     }
 
     @Override
     public void publish(LikeEvent.LikeCanceled event) {
-        kafkaTemplate.send(LikeEvent.TOPIC.LIKE_CANCELED, event.productId().toString(), event);
+        LikeGlobalEvent.Canceled kafkaEvent =
+                new LikeGlobalEvent.Canceled(event.eventId(), event.productId(), event.userId(), ZonedDateTime.now());
+        kafkaTemplate.send(LikeGlobalEvent.TOPIC.LIKE_CANCELED, kafkaEvent.productId().toString(), kafkaEvent);
     }
 }
