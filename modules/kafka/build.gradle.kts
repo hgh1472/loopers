6efe551fcc205f590947f659ce3539c183ab1253
hgh1472 <hgh1472@naver.com>
2025-09-05 03:44:01 +0900

    chore: Parallel Consumer 의존성 추가

--- modules/kafka/build.gradle.kts
+++ modules/kafka/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 dependencies {
     api("org.springframework.kafka:spring-kafka")
+    api("io.confluent.parallelconsumer:parallel-consumer-core:0.5.3.3")
 
     testImplementation("org.springframework.kafka:spring-kafka-test")
     testImplementation("org.testcontainers:kafka")
