1657cd498b1185685f71a06148d14c511acaf2b3
hgh1472 <hgh1472@naver.com>
2025-09-18 23:16:53 +0900

    refactor: MV 저장 JDBC로 변경

--- apps/commerce-batch/src/main/java/com/loopers/infrastructure/ranking/RankMvRepositoryImpl.java
+++ apps/commerce-batch/src/main/java/com/loopers/infrastructure/ranking/RankMvRepositoryImpl.java
@@ -4,9 +4,11 @@ import com.loopers.domain.ranking.MonthlyProductRankMv;
 import com.loopers.domain.ranking.RankMvRepository;
 import com.loopers.domain.ranking.WeeklyProductRankMv;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,15 +16,36 @@ import org.springframework.stereotype.Repository;
 public class RankMvRepositoryImpl implements RankMvRepository {
     private final WeeklyProductRankMvJpaRepository weeklyJpaRepository;
     private final MonthlyProductRankMvJpaRepository monthlyJpaRepository;
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
     public void saveWeeklyRankingMvs(Iterable<WeeklyProductRankMv> entities) {
-        weeklyJpaRepository.saveAll(entities);
+        String sql = "INSERT INTO mv_product_rank_weekly (ref_product_id, weekly_rank, score, date) VALUES (?, ?, ?, ?)";
+        List<Object[]> batchArgs = new ArrayList<>();
+        for (WeeklyProductRankMv entity : entities) {
+            batchArgs.add(new Object[]{
+                    entity.getProductId(),
+                    entity.getRank(),
+                    entity.getScore(),
+                    entity.getDate()
+            });
+        }
+        jdbcTemplate.batchUpdate(sql, batchArgs);
     }
 
     @Override
     public void saveMonthlyRankingMvs(Iterable<MonthlyProductRankMv> entities) {
-        monthlyJpaRepository.saveAll(entities);
+        String sql = "INSERT INTO mv_product_rank_monthly (ref_product_id, monthly_rank, score, date) VALUES (?, ?, ?, ?)";
+        List<Object[]> batchArgs = new ArrayList<>();
+        for (MonthlyProductRankMv entity : entities) {
+            batchArgs.add(new Object[]{
+                    entity.getProductId(),
+                    entity.getRank(),
+                    entity.getScore(),
+                    entity.getDate()
+            });
+        }
+        jdbcTemplate.batchUpdate(sql, batchArgs);
     }
 
     @Override
